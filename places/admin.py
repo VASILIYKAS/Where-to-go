afb6576cf11315875f8f9db456fd0b155677fcf1
VASILIYKAS <yoyoynv@gmail.com>
2025-05-06 19:10:07 +0500

    Add photo upload functionality to Place model in admin

--- places/admin.py
+++ places/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from .models import Place, PlaceImage
 
 
-admin.site.register(Place)
-admin.site.register(PlaceImage)
+class PlaceImageInline(admin.TabularInline):
+    model = PlaceImage
+    extra = 1
+    fields = ('image', 'position')
+    readonly_fields = ('image_info',)
+
+    def image_info(self, obj):
+        if obj.pk and obj.image:
+            return obj.image.name.split('/')[-1]
+        return "Нет файла"
+
+
+@admin.register(Place)
+class PlaceAdmin(admin.ModelAdmin):
+    inlines = [PlaceImageInline]
+    list_display = ('title',)
